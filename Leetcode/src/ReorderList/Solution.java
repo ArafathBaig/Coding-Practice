c82965bcb3f5787f80441c28fc97770aee0883c2
Arafath Baig <arafath.moghul@gmail.com>
2021-07-29 11:09:44 +0530

    reorder list

--- Leetcode/src/ReorderList/Solution.java
+++ Leetcode/src/ReorderList/Solution.java
@@ -12,50 +12,44 @@ class Solution {
         ListNode slow = head;
         ListNode fast = head;
         ListNode prev = null;
-
         do {
             prev = slow;
             slow = slow.next;
             fast = fast.next.next;
         } while (fast != null && fast.next != null);
 
-        ListNode temp = null;
-        if (fast == null) {
-            temp = slow;
-            prev.next = null;
-        } else {
-            temp = slow.next;
-            slow.next = null;
+        prev.next = null;
+        slow = reverse(slow);
+
+        ListNode nextTemp = head;
+        ListNode headNext = null;
+        ListNode next = null;
+        while (slow != null) {
+
+            next = nextTemp.next;
+            nextTemp.next = slow;
+
+            slow = next;
+            nextTemp = nextTemp.next;
         }
 
-        slow = reverse(temp);
-
-        ListNode currNext = null;
-        ListNode slowNext = null;
-
-        ListNode curr = head;
-        while (curr != null && slow != null) {
-            currNext = curr.next;
-            slowNext = slow.next;
-            curr.next = slow;
-            slow.next = currNext;
-
-            curr = currNext;
-            slow = slowNext;
-        }
+        if (slow != null)
+            nextTemp.next = slow;
 
     }
 
     private ListNode reverse(ListNode head) {
 
         ListNode next = null;
-        ListNode prev = null;
         ListNode curr = head;
+        ListNode prev = null;
 
         while (curr != null) {
+
             next = curr.next;
             curr.next = prev;
             prev = curr;
+
             curr = next;
         }
 
